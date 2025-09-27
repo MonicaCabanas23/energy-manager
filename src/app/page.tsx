db46f0bb9d3f5ec378678875e3f3da03e3f83de8
MonicaCabanas23 <00079921@uca.edu.sv>
2025-09-27 17:56:38 -0600

    :heavy_plus_sign: adding prisma for db actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import { auth0 } from "@/lib/auth0";
+import { prisma } from "@/lib/prisma";
 
 export default async function Home() {
   // Fetch the user session
   const session = await auth0.getSession();
 
+  const users = await prisma.user.findMany()
+
   // If session exists, show a welcome message and logout button
   return (
     <main>
       <h1>Welcome, {session?.user.name}!</h1>
+      {
+        users.map((user:any) => (<p key={user.id}>{user.name}</p>))
+      }
     </main>
   );
 }
